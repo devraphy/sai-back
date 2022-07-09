5034cb44b7ee00173851f1190db403ce83b83df6
devraphy <devraphy@gmail.com>
2022-07-09 14:29:09 +0900

    first commit - setting development environment

---
+++ src/test/java/projectsai/saibackend/SaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projectsai.saibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
