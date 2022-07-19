2d6abc08378e3932eb8d0661c562da5cfcfe761c
devraphy <devraphy@gmail.com>
2022-07-19 15:16:13 +0900

    Member Entity was created

--- src/main/java/projectsai/saibackend/domain/Member.java
+++ src/main/java/projectsai/saibackend/domain/Member.java
@@ -3,10 +3,9 @@ package projectsai.saibackend.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity @Getter @Setter
 public class Member {
@@ -14,9 +13,14 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @Column(name = "member_name")
+    @Column(name = "member_name", nullable = false)
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    private LocalDate signUpDate;
 }
