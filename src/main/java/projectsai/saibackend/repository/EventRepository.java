cd0a6b1d596c62a0d781d53157c2b4e93373a18a
devraphy <devraphy@gmail.com>
2022-07-26 17:44:48 +0900

    Optimizing EventRepository methods

--- src/main/java/projectsai/saibackend/repository/EventRepository.java
+++ src/main/java/projectsai/saibackend/repository/EventRepository.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Repository;
 import projectsai.saibackend.domain.Event;
 import projectsai.saibackend.domain.Friend;
 import projectsai.saibackend.domain.Member;
+import projectsai.saibackend.domain.enums.EventEvaluation;
+import projectsai.saibackend.domain.enums.EventPurpose;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -16,34 +19,46 @@ public class EventRepository {
     private EntityManager em;
 
     // CREATE
-    public Long save(Event event) {
-        em.persist(event);
-        return event.getId();
-    }
 
     // READ
-    public Event findById(Long id) {
-        return em.find(Event.class, id);
-    }
-
-    public List<Event> findByName(String name, Member member_id) {
-        return em.createQuery("select e from Event e where e.eventName = :name and e.owner = :member_id", Event.class)
-                .setParameter("name", name)
-                .setParameter("member_id", member_id)
+    public List<Event> findAll(Member owner) {
+        return em.createQuery("select e from Event e where e.owner = :owner", Event.class)
+                .setParameter("owner", owner)
                 .getResultList();
     }
 
-    public List<Event> findByFriend(Friend friend_id, Member member_id) {
-        return em.createQuery("select e from Event e where e.participants = :friend_id and e.owner = :member_id", Event.class)
-                .setParameter("friend_id", friend_id)
-                .setParameter("member_id", member_id)
+    public List<Event> findByFriend(Member owner, List<Friend> friendList) { // 이거 제대로 동작하는지 검증하기
+        return em.createQuery("select e from Event e where e.owner = :owner and e.participants = :friendList", Event.class)
+                .setParameter("owner", owner)
+                .setParameter("friendList", friendList)
                 .getResultList();
     }
 
-    public List<Event> findAll(Member member_id) {
-        return em.createQuery("select e from Event e where e.owner = :member_id", Event.class)
-                .setParameter("member_id", member_id)
+    public List<Event> findByEventName(Member owner, String eventName) {
+        return em.createQuery("select e from Event e where e.owner = :owner and e.eventName = :eventName", Event.class)
+                .setParameter("owner", owner)
+                .setParameter("eventName", eventName)
                 .getResultList();
     }
 
+    public List<Event> findByDate(Member owner, LocalDate date) {
+        return em.createQuery("select e from Event e where e.owner = :owner and e.eventDate = :date", Event.class)
+                .setParameter("owner", owner)
+                .setParameter("date", date)
+                .getResultList();
+    }
+
+    public List<Event> findByPurpose(Member owner, EventPurpose purpose) {
+        return em.createQuery("select e from Event e where e.owner = :owner and e.eventPurpose = :purpose", Event.class)
+                .setParameter("owner", owner)
+                .setParameter("purpose", purpose)
+                .getResultList();
+    }
+
+    public List<Event> findByEvaluation(Member owner, EventEvaluation evaluation) {
+        return em.createQuery("select e from Event e where e.owner = :owner and e.evaluation = :evaluation", Event.class)
+                .setParameter("owner", owner)
+                .setParameter("evaluation", evaluation)
+                .getResultList();
+    }
 }
