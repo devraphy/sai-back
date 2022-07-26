ba0a10a3ef8493bc8a790a9a8a153ea51d2b8d1d
devraphy <devraphy@gmail.com>
2022-07-26 17:43:46 +0900

    implemented Event List, and business method to inject Member_id in Event Entity

--- src/main/java/projectsai/saibackend/domain/Member.java
+++ src/main/java/projectsai/saibackend/domain/Member.java
@@ -26,13 +26,21 @@ public class Member {
     @NotNull
     private LocalDate signUpDate;
 
-//    Member 객체를 생성하고 그 멤버 객체의 addFriend 기능을 이용해서 Friend를 저장해야한다.
+    // Member 객체를 생성하고 그 멤버 객체의 addFriend 기능을 이용해서 Friend를 저장해야한다.
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
     private List<Friend> friendList = new ArrayList<>();
 
     public void addFriend(Friend friend) {
         this.friendList.add(friend);
         friend.setOwner(this);
+    }
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Event> eventList = new ArrayList<>();
+
+    public void addEvent(Event event) {
+        this.eventList.add(event);
+        event.setOwner(this);
     }
 
     public Member() {}
