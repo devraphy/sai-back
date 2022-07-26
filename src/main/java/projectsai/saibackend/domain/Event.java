ee9f9eb2c73db5668148db794a0e7e978cb77177
devraphy <devraphy@gmail.com>
2022-07-26 17:42:31 +0900

    completed setting Event Entity

--- src/main/java/projectsai/saibackend/domain/Event.java
+++ src/main/java/projectsai/saibackend/domain/Event.java
@@ -1,16 +1,17 @@
 package projectsai.saibackend.domain;
 
 import lombok.Getter;
-import lombok.Setter;
-import projectsai.saibackend.domain.enums.Evaluation;
+import org.springframework.lang.Nullable;
+import projectsai.saibackend.domain.enums.EventEvaluation;
 import projectsai.saibackend.domain.enums.EventPurpose;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity @Getter @Setter
+@Entity @Getter
 public class Event {
 
     @Id @GeneratedValue
@@ -18,22 +19,38 @@ public class Event {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member owner;
 
+    @NotNull
     private LocalDate eventDate;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) @NotNull
     private EventPurpose eventPurpose;
 
-    @Column(name = "event_name")
+    @Column(name = "event_name") @Nullable
     private String eventName;
 
-    @Enumerated(EnumType.STRING)
-    private Evaluation evaluation;
+    @Enumerated(EnumType.STRING) @NotNull
+    private EventEvaluation evaluation;
 
-    // 이거 돌려보고 DB 구조 확인해볼 것
     @ManyToMany
     @JoinTable(name = "event_participants")
     private List<Friend> participants = new ArrayList<>();
+
+    // 연관 관계 메서드
+    public void setOwner(Member member) {
+        this.owner = member;
+    }
+
+    public Event() {}
+
+    public Event(LocalDate eventDate, EventPurpose eventPurpose, @Nullable String eventName,
+                 EventEvaluation evaluation, List<Friend> participants) {
+        this.eventDate = eventDate;
+        this.eventPurpose = eventPurpose;
+        this.eventName = eventName;
+        this.evaluation = evaluation;
+        this.participants = participants;
+    }
 }
