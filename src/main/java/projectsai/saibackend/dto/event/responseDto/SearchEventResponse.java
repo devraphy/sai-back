45863013681549be95d994abcf3f33fafc25cde4
devraphy <devraphy@gmail.com>
2022-08-13 13:17:43 +0900

    Optimized response dto

--- src/main/java/projectsai/saibackend/dto/event/responseDto/SearchEventResponse.java
+++ src/main/java/projectsai/saibackend/dto/event/responseDto/SearchEventResponse.java
@@ -4,30 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import projectsai.saibackend.domain.Event;
 import projectsai.saibackend.domain.Friend;
-import projectsai.saibackend.domain.Member;
-import projectsai.saibackend.domain.enums.EventEvaluation;
-import projectsai.saibackend.domain.enums.EventPurpose;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @Data @AllArgsConstructor
 public class SearchEventResponse {
-    private Long eventId;
-    private Member owner;
-    private LocalDate date;
-    private String name;
-    private EventPurpose purpose;
-    private EventEvaluation evaluation;
+    private Event event;
     private List<Friend> participants;
-
-    public SearchEventResponse(Event event, List<Friend> participants) {
-        this.eventId = event.getId();
-        this.owner = event.getOwner();
-        this.date = event.getDate();
-        this.name = event.getName();
-        this.purpose = event.getPurpose();
-        this.evaluation = event.getEvaluation();
-        this.participants = participants;
-    }
 }
