36b9ecd16f3adf10d4569638ea09af09be9a725b
devraphy <devraphy@gmail.com>
2022-08-24 00:06:08 +0900

    Changed property name from authority to role

--- src/main/java/projectsai/saibackend/domain/Member.java
+++ src/main/java/projectsai/saibackend/domain/Member.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity @Getter
-public class Member implements UserDetails {
+public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long memberId;
@@ -34,17 +34,17 @@ public class Member implements UserDetails {
     private Boolean visibility;
 
     @NotNull
-    private String authority;
+    private String role;
 
     // Constructor
     public Member() {}
 
-    public Member(String name, String email, String password, Boolean visibility, String authority) {
+    public Member(String name, String email, String password, Boolean visibility, String role) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.visibility = visibility;
-        this.authority = authority;
+        this.role = role;
     }
 
     // Business Methods
@@ -57,44 +57,4 @@ public class Member implements UserDetails {
     public void deleteMember() {
         this.visibility = Boolean.TRUE;
     }
-
-
-    // UserDetails
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        ArrayList<GrantedAuthority> auth = new ArrayList<>();
-        auth.add(new SimpleGrantedAuthority(authority));
-        return auth;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
 }
