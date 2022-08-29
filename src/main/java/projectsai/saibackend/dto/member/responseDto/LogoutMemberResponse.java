84cbb5f7c744daee308d5b44c08134fbb1d6455a
devraphy <devraphy@gmail.com>
2022-08-29 15:25:45 +0900

    Response DTO for logout request

---
+++ src/main/java/projectsai/saibackend/dto/member/responseDto/LogoutMemberResponse.java
@@ -0,0 +1,11 @@
+package projectsai.saibackend.dto.member.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class LogoutMemberResponse {
+    private Boolean result;
+}
