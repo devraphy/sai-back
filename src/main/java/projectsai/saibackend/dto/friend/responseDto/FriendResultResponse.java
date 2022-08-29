a72ca18560863e86823399f0521a79165651cbbe
devraphy <devraphy@gmail.com>
2022-08-29 18:12:02 +0900

    Added response DTO for returning Boolean usage

---
+++ src/main/java/projectsai/saibackend/dto/friend/responseDto/FriendResultResponse.java
@@ -0,0 +1,9 @@
+package projectsai.saibackend.dto.friend.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class FriendResultResponse {
+    private Boolean result;
+}
