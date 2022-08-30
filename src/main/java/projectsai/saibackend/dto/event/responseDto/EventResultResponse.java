86566364c694c6003a53b0919c2780f60e0de4da
devraphy <devraphy@gmail.com>
2022-08-30 18:32:03 +0900

    Implemented new response DTO to return Boolean type

---
+++ src/main/java/projectsai/saibackend/dto/event/responseDto/EventResultResponse.java
@@ -0,0 +1,9 @@
+package projectsai.saibackend.dto.event.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class EventResultResponse {
+    private Boolean result;
+}
