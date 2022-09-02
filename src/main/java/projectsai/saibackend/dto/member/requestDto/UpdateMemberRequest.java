7d6f44882df2818a58eb6e7ce28cf5d37563337b
devraphy <devraphy@gmail.com>
2022-09-02 15:24:58 +0900

    Added api model annotation for swagger use

--- src/main/java/projectsai/saibackend/dto/member/requestDto/UpdateMemberRequest.java
+++ src/main/java/projectsai/saibackend/dto/member/requestDto/UpdateMemberRequest.java
@@ -1,12 +1,16 @@
 package projectsai.saibackend.dto.member.requestDto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data @NotNull
 public class UpdateMemberRequest {
+    @ApiModelProperty(example = "변경할 이메일")
     private String email;
+    @ApiModelProperty(example = "변경할 이름")
     private String name;
+    @ApiModelProperty(example = "변경할 비밀번호")
     private String password;
 }
