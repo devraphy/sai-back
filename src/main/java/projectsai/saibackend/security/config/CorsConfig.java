bab514137bb0f7b4f00626f68c5585063012684d
devraphy <devraphy@gmail.com>
2022-09-04 00:48:05 +0900

    Edited a comment

--- src/main/java/projectsai/saibackend/security/config/CorsConfig.java
+++ src/main/java/projectsai/saibackend/security/config/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 서버의 response를 js로 요청 및 처리 가능하게 할지의 여
-        config.addAllowedOrigin("*"); // 모든 IP에 응답을 허용
+        config.setAllowCredentials(true); // 서버의 response를 js로 요청 및 처리 가능하게 할지의 여부
+        config.addAllowedOrigin("*"); // 모든 URL의 요청을 허용
         config.addAllowedHeader("*"); // 모든 Header에 응답을 허용
         config.addAllowedMethod("*"); // 모든 Http method를 허용
         source.registerCorsConfiguration("/api/**", config);
